0730519bdfc573f961015f199beda7927193d53f
yeongbi choe <cyeongb@gmail.com>
2021-03-06 09:43:49 +0900

    2021-03-06 09:43 update;

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,8 +1,8 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { RiMenuAddFill } from 'react-icons/ri'; //https://react-icons.github.io/react-icons/icons?name=md
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
 
   const change = useCallback((e) => {
@@ -10,9 +10,24 @@ const TodoInsert = () => {
     setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+      //submit 이벤트가 새로고침을 발생시키기때문에 브라우저 update방지로 preventDefault()를 호출합니다.
+    },
+    [onInsert, value],
+  );
+
   return (
     <form className="TodoInsert">
-      <input placeholder="input your plan" value={value} onChange={change} />
+      <input
+        placeholder="input your plan"
+        value={value}
+        onChange={change}
+        onSubmit={onSubmit}
+      />
       <button type="submit">
         <RiMenuAddFill />
       </button>
